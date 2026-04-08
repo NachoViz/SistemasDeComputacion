544e8f29499b080ddcaa7e7a2fd1c2dc6bb5d085
Nacho Vizgarra <juan.vizgarra@mi.unc.edu.ar>
2026-04-08 19:19:11 -0300

    Agregada capa intermedia en C para puente con Assembler junto con su test

---
+++ TP2/app/interm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Declaramos la función que vamos a escribir en Assembler.
+// Recibe un double (64 bits) y devuelve un long (64 bits).
+extern long procesar_gini_asm(double valor);
+
+// Esta función es la que llamará Python usando ctypes.
+long puente_python_asm(double valor_gini) {
+    // Aquí llamamos a la rutina de bajo nivel.
+    long resultado = procesar_gini_asm(valor_gini);
+    return resultado;
+}
